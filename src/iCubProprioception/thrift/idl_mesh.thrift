c0a67ce4a8096300566daf5f5058b125f4c57009
Claudio <claudio.fantacci@gmail.com>
2017-01-23 20:53:12 +0100

    First commit

---
+++ src/iCubProprioception/thrift/idl_mesh.thrift
@@ -0,0 +1,36 @@
+# Copyright: (C) 2016 iCub Facility - Istituto Italiano di Tecnologia
+# Authors: Claudio Fantacci
+#
+# idl_mesh.thrift
+
+/**
+ * iCubProprioceptionOGLIDL
+ *
+ * IDL Interface to \ref iCubProprioception OpenGL services.
+ */
+
+service iCubProprioceptionOGLIDL
+{
+    /**
+     * View or hide background image of the mesh window.
+     * @param status true/false to turn background on/off.
+     * @return true activation/deactivation success, false otherwise.
+     */
+    bool mesh_background(1:bool status);
+
+    /**
+     * Render the mesh as a wireframe. If disabled (default behaviour)
+     * the mesh is filled without light.
+     * @param status true/false to turn wireframe rendering on/off.
+     * @return true activation/deactivation success, false otherwise.
+     */
+    bool mesh_wireframe(1:bool status);
+
+    /**
+     * Set mipmaps color filtering technique.
+     * Default is "nearest" (i.e. nearest neighbor).
+     * @param type can be "nearest" (default) or "linear".
+     * @return true on correct activation, false otherwise.
+     */
+    bool mesh_mipmaps(1:string type);
+}
