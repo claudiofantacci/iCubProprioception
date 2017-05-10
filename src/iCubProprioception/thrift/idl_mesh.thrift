1dde1ccfaca52e20ea2305c6b5670f19bf304193
Claudio <claudio.fantacci@gmail.com>
2017-05-10 17:24:35 +0200

    Add new sync_input() Thrift service with documentation

--- src/iCubProprioception/thrift/idl_mesh.thrift
+++ src/iCubProprioception/thrift/idl_mesh.thrift
@@ -13,7 +13,9 @@ service iCubProprioceptionOGLIDL
 {
     /**
      * View or hide background image of the mesh window.
+     *
      * @param status true/false to turn background on/off.
+     *
      * @return true activation/deactivation success, false otherwise.
      */
     bool mesh_background(1:bool status);
@@ -21,8 +23,23 @@ service iCubProprioceptionOGLIDL
     /**
      * Render the mesh as a wireframe. If disabled (default behaviour)
      * the mesh is filled without light.
+     *
      * @param status true/false to turn wireframe rendering on/off.
+     *
      * @return true activation/deactivation success, false otherwise.
      */
     bool mesh_wireframe(1:bool status);
+
+    /**
+     * Synchronize rendering FPS to the input rate of the poses received from
+     * the ports.
+     *
+     * If the synchronization is off, then the last received pose is used by
+     * the rendered (pose lock).
+     *
+     * @param status true/false to turn synchronization on/off.
+     *
+     * @return true activation/deactivation success, false otherwise.
+     */
+    bool sync_input(1:bool status);
 }
