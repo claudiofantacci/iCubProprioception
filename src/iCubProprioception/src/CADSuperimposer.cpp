470da24cb2eadb2bc44ce9714a389e8796b6d8d2
Claudio Fantacci <claudio.fantacci@gmail.com>
2018-07-13 19:01:04 +0200

    Update to new SuperimposeMesh lib APIs

--- src/iCubProprioception/src/CADSuperimposer.cpp
+++ src/iCubProprioception/src/CADSuperimposer.cpp
@@ -140,9 +140,8 @@ CADSuperimposer::CADSuperimposer(const yarp::os::ConstString& robot, const yarp:
     drawer_ = new SICAD(cad_hand_,
                         cam_width_, cam_height_, cam_fx_, cam_fy_, cam_cx_, cam_cy_,
                         1,
-                        {1.0, 0.0, 0.0, static_cast<float>(M_PI)},
                         shader_path_,
-                        false);
+                        {1.0, 0.0, 0.0, static_cast<float>(M_PI)});
 
     drawer_->setBackgroundOpt(true);
     drawer_->setWireframeOpt(true);
